1603cbbb2255f1e3c4d9a022547e44d094a5286d
Daniela-DG <danieladiazgaviria03@gmail.com>
2021-05-14 10:37:07 -0500

    Creacion de las sugerencias en la api

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
- 
+const config = require('./config')
 const connectToDB = () => {
 
     mongoose.connect('mongodb://127.0.0.1:27017/NoticiasTW', { useNewUrlParser: true,
--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const {connectToDB} = require ('./db')
 
 const app = express()
 
+// el la clase 1:10 para aplicarlo del dia 11/05 app.use(corse)
 app.use(bodyParser.json())
 connectToDB()
 
 require('./Routes/Nacionales') (app)
 require('./Routes/Internacionales') (app)
+require('./Routes/Sugerencia')
 
 app.listen(3000, () =>{
     console.log('La conexion al puerto fue exitosa')
