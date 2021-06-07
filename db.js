ed28c6e00728025cebfdcfeaba4568813fe82296
Daniela-DG <danieladiazgaviria03@gmail.com>
2021-06-06 20:24:05 -0500

    La api se encuentra de forma local

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const config = require('./config')
 const connectToDB = () => {
 
-    mongoose.connect(config.mongoDB , { useNewUrlParser: true,
+    mongoose.connect('mongodb://127.0.0.1:27017/NoticiasTW', { useNewUrlParser: true,
      useUnifiedTopology: true }, (error) => {
         if(error){
             console.log('Tenemos un error en la conexion a la DB', error)
